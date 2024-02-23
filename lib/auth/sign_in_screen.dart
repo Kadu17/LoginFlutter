df00b30eca152aa7534a311763b9237fc7cc117b
Kadu17 <50947025804>
2024-02-23 16:52:10 -0300

    terminando tela de login e colocando animação

--- lib/auth/sign_in_screen.dart
+++ lib/auth/sign_in_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterlogin/auth/components/custom_text_field.dart';
 
 class SigInScreen extends StatelessWidget {
   const SigInScreen({Key? key}) : super(key: key);
@@ -6,42 +8,158 @@ class SigInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.lightBlue,
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // nome do app
+               const Text.rich(TextSpan(
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'SNKRS',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ) 
+                    ),
+                    TextSpan(
+                      text: 'NTC',
+                      style: TextStyle(
+                        color: Colors.white
+                      )
+                    )
+                  ]
+                )),
+                
+                //categorias
+                SizedBox(
+                  height: 30,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                    child: AnimatedTextKit(
+                      pause: Duration.zero,
+                      repeatForever: true,
+                      animatedTexts: [
+                        FadeAnimatedText('STYLE'),
+                        FadeAnimatedText('SNKRS'),
+                        FadeAnimatedText('MODELS'),
+                        FadeAnimatedText('NTC'),
+                      ],),
+                  ),
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 40,
-              ),
-              decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(45),
-              ),
-              ),
-              child: Column(
-                children: [
-                  
-                  TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
+
+          //formulario
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(45),
+            ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Email
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                // Senha
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,  
+                ),
+          
+                // Botão para entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular (18),
+                      ),
+                    ),
+                    onPressed: (){},
+                    child: const Text('Entrar', style: TextStyle(
+                      fontSize: 18,
+                    ),),
+                  ),
+                ), 
+          
+                //esqueceu a senha
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(onPressed: () {}, 
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(
+                        color: Colors.black
                       ),
                     ),
                   ),
-
-
-                  TextFormField(),
-                ],
-              )          
-            ),
+                ),
+                //divisor
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.black.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('Ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.black.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+          
+                //Botão de novo usuario
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.blue
+                      )
+                    ),
+                    onPressed: () {},
+                    child: const Text('Criar conta', style: TextStyle(
+                      fontSize: 18,
+                    ),),
+                  ),
+                ),
+              ],
+            ),          
           ),    
         ],
       ),
